1c084cdc3021b842ca07ec2f032ef12cacf6ff2c
Gnurro <knawurzelkopp@hotmail.de>
2021-03-17 18:20:10 +0100

    fixed persistent object checks; lotsa ? added

--- Encounters/contextModifier.js
+++ Encounters/contextModifier.js
@@ -4,9 +4,9 @@ const modifier = (text) => {
     // for mashing with other context scripts, keep this codeblock before 'const contextMemory'
     // encounter memory stuff:
     if (state.encounterPersistence) {
-        if (state.encounterPersistence.memories) {
+        if (state.encounterPersistence?.memories) {
             memoryLoop:
-                for (let encounterMemory of state.encounterPersistence.memories) {
+                for (let encounterMemory of state.encounterPersistence?.memories) {
                     // take care to not overload memory, unless forced:
                     if (!encounterMemory.memoryGreed && (encounterMemory.memoryText.length + memory.length) > 1000) {
                         encounterLog(`Non-greedy encounterMemory too long, not inserting it!`)
@@ -34,7 +34,7 @@ const modifier = (text) => {
     // for mashing with other context scripts, keep this between 'const lines' and 'const combinedLines'
     if (typeof (state.currentEncounter) !== 'undefined') { // if there's an event...
         if (state.encounterPersistence) {
-            lines.splice(-3, 0, state.encounterPersistence.contextNote) // ...put it right below AN, so AI knows what's up
+            lines.splice(-3, 0, state.encounterPersistence?.contextNote) // ...put it right below AN, so AI knows what's up
         }
     }
     // END Encounters
