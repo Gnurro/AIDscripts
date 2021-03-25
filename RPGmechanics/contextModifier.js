5f7164307a47f827e6a0fdb335374d47180cfa56
Gnurro <knawurzelkopp@hotmail.de>
2021-03-25 16:29:31 +0100

    started contextMod overhaul; moving stat results to statConfig; reworked check stuff

--- RPGmechanics/contextModifier.js
+++ RPGmechanics/contextModifier.js
@@ -23,20 +23,26 @@ const modifier = (text) => {
         state.displayStats = [{key: 'XP', value: state.RPGstate.XP, color: 'green'}] // show current XP
     }
 
+    /*
     // reading in stats from menu:
     // TODO: make this work based on statConfig || smarter?
+    // TODO: just integrate into check below!
     intMod = state.stats["stats"]["Intelligence"]["level"]
     chaMod = state.stats["stats"]["Charisma"]["level"]
     wisMod = state.stats["stats"]["Wisdom"]["level"]
     strMod = state.stats["stats"]["Strength"]["level"]
     dexMod = state.stats["stats"]["Dexterity"]["level"]
     conMod = state.stats["stats"]["Constitution"]["level"]
+    */
 
     if (info.actionCount > 1 && state.inputBot) {
         RPGmechsLog(info?.inputEvaluation) // log what the bot came up with
 
+        // store bot output:
+        let botOutput = info?.inputEvaluation
+
         // put bot output into handy variables:
-        let chkAtt = info?.inputEvaluation[statConfig.botOutputs.stat]
+        let chkStat = info?.inputEvaluation[statConfig.botOutputs.stat]
         let chkDC = info?.inputEvaluation[statConfig.botOutputs.dc]
         let chkCuz = info?.inputEvaluation[statConfig.botOutputs.cuz]
 
@@ -44,57 +50,67 @@ const modifier = (text) => {
         delete state.inputBot
 
         // use difficulty to scale XP:
+        // TODO: make this a framework option
         let chkXP = chkDC / 5 // DC are divisible by five, so this probably works
 
         // optional DC display:
+        // TODO: make this more configurable; framework options
         if (state.showDC) { // if the display is on (iE, state.showDC == true)...
-            state.message = statConfig.statList[chkAtt].icon + " DC " + chkDC + ": " + chkCuz // ...show the attribute, DC and reason in state.message
+            state.message = statConfig.statList[chkStat].icon + " DC " + chkDC + ": " + chkCuz // ...show the attribute, DC and reason in state.message
         } else { // if the display is off (iE, state.showDC == false)...
             state.message = chkCuz // ...show only the reason
         }
 
-
         // get check attribute from bot output and assign appropriate attribute modifiers:
-        if (chkAtt != null) { // if the bot does come up with anything
-            if (chkAtt.includes("Any")) { // bot sometimes gives that one; just take it as 'too generic'
+        checkBit:
+        if (chkStat != null) { // if the bot does come up with anything
+            if (chkStat.includes("Any")) { // bot sometimes gives that one; just take it as 'too generic'
                 chkCurAtt = 0 // so it gets no attribute bonus
                 chkAttPosAdj = "good" // this is the crucial bit for generation, but since the bot said it's generic...
                 chkAttNegAdj = "bad" // ...AI is told generic things below
+                break checkBit
             }
 
+            // get the corresponding modifier from stat menu:
+            chkCurAtt = state.stats.stats[chkStat].level
 
+            // get adjectives from statConfig:
+            chkAttPosAdj = statConfig.statList[chkStat.toLowerCase()].successAdjective // this is the crucial bit for generation, but since the bot said it's generic...
+            chkAttNegAdj = statConfig.statList[chkStat.toLowerCase()].failAdjective // ...AI is told generic things below
 
-            if (chkAtt.includes("Intelligence")) { // when the bot comes up with an attribute...
+            /*
+            if (chkStat.includes("Intelligence")) { // when the bot comes up with an attribute...
                 chkCurAtt = intMod // ...assign the appropriate attribute modifier...
                 chkAttPosAdj = "smart" // ...and use a fitting positive word...
                 chkAttNegAdj = "dumb" // ...or negative word to let the AI know for generation below
             }
             // same as above, for all attributes:
-            if (chkAtt.includes("Wisdom")) {
+            if (chkStat.includes("Wisdom")) {
                 chkCurAtt = wisMod
                 chkAttPosAdj = "wise"
                 chkAttNegAdj = "oblivious"
             }
-            if (chkAtt.includes("Charisma")) {
+            if (chkStat.includes("Charisma")) {
                 chkCurAtt = chaMod
                 chkAttPosAdj = "charming"
                 chkAttNegAdj = "annoying"
             }
-            if (chkAtt.includes("Strength")) {
+            if (chkStat.includes("Strength")) {
                 chkCurAtt = strMod
                 chkAttPosAdj = "strong"
                 chkAttNegAdj = "weak"
             }
-            if (chkAtt.includes("Dexterity")) {
+            if (chkStat.includes("Dexterity")) {
                 chkCurAtt = dexMod
                 chkAttPosAdj = "nimble"
                 chkAttNegAdj = "clumsy"
             }
-            if (chkAtt.includes("Constitution")) {
+            if (chkStat.includes("Constitution")) {
                 chkCurAtt = conMod
                 chkAttPosAdj = "tough"
                 chkAttNegAdj = "scrawny"
             }
+            */
 
             // skill handling:
             if (typeof (state.RPGstate.chkSitBonus) !== 'undefined') { // if there's a skill bonus...
@@ -103,7 +119,7 @@ const modifier = (text) => {
                 // get skill-dependent result strings:
                 for (let skillDef in skillDB) {
                     if (skillDef === state.chkSitSkill) {
-                        console.log("found skillDef for current skill:" + skillDef)
+                        RPGmechsLog("found skillDef for current skill:" + skillDef)
                         if (skillDB[skillDef].overrideAtt === true) {
                             overrideAtt = true
                             chkSkillPosStr = skillDB[skillDef].results['positive']
@@ -123,10 +139,11 @@ const modifier = (text) => {
 
 
             // Feats handling:
-
+            /*
             for (feat of state.charFeats) {
                 console.log(feat)
             }
+            */
 
 
             // the actual check:
@@ -135,10 +152,10 @@ const modifier = (text) => {
             if (chkModRoll >= chkDC) { // if the result beats the DC...
                 chkResult = "Success!" // ...put the result in words for the player...
                 if (typeof (chkSkillPosStr) !== 'undefined') {
-                    if (overrideAtt == true) {
+                    if (overrideAtt === true) {
                         resultContextString = `[${chkSkillPosStr}]` // ...and in words for the AI, as well,...
                     }
-                    if (overrideAtt == false) {
+                    if (overrideAtt === false) {
                         resultContextString = `[You are ${chkAttPosAdj} enough for that right now, and ${chkSkillPosStr}.]` // ...and in words for the AI, as well,...
                     }
                 } else {
@@ -148,10 +165,10 @@ const modifier = (text) => {
             } else { // if the result does NOT beat the DC...
                 chkResult = "Fail!" // ...put the result in words for the player...
                 if (typeof (chkSkillNegStr) !== 'undefined') {
-                    if (overrideAtt == true) {
+                    if (overrideAtt === true) {
                         resultContextString = `[${chkSkillNegStr}]` // ...and in words for the AI, as well,...
                     }
-                    if (overrideAtt == false) {
+                    if (overrideAtt === false) {
                         resultContextString = `[You are too ${chkAttNegAdj} for that right now, and ${chkSkillNegStr}.]` // ...and in words for the AI, as well,...
                     }
                 } else {
@@ -168,7 +185,7 @@ const modifier = (text) => {
 
             // display the full line at the bottom:
             if (info.actionCount >= 2) { // but only if it's useful and possible
-                state.message += ` ${chkAtt} roll: ${chkModRoll} (${roll}${makeModString(chkCurAtt)}${makeModString(state.chkSitBonus)}), ${chkResult} XP gained: ${chkXP}` // add all the check stuff to the message
+                state.message += ` ${chkStat} roll: ${chkModRoll} (${roll}${makeModString(chkCurAtt)}${makeModString(state.chkSitBonus)}), ${chkResult} XP gained: ${chkXP}` // add all the check stuff to the message
             }
 
             // clean up:
@@ -178,9 +195,11 @@ const modifier = (text) => {
             }
         }
 
+        /*
         for (feat of state.charFeats) {
             console.log(feat)
         }
+        */
 
     }
     // END rpg mechanics
