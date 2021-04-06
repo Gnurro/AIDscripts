574fc095ec3cddd5117eef8e28e11573b5778936
Gnurro <knawurzelkopp@hotmail.de>
2021-04-06 17:34:03 +0200

    turned off hardLog for now

--- RPGmechanics/shared.js
+++ RPGmechanics/shared.js
@@ -24,7 +24,7 @@ const miscConfig = {
     showFancyHP: true,
     showResources: true,
     doLog: true,
-    hardLog: true
+    hardLog: false
 }
 
 // MANDATORY generic character sheet initializer:
