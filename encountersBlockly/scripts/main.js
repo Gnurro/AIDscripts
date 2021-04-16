c35148c8a760fce08329c8de4167edf96525ea76
Gnurro <knawurzelkopp@hotmail.de>
2021-04-16 16:13:39 +0200

    updated Encounters blockly with workspace export

--- encountersBlockly/scripts/main.js
+++ encountersBlockly/scripts/main.js
@@ -34,6 +34,46 @@ function loadBlocklySpace() {
 	Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);	
 }
 
+function exportBlocklySpace(filename) {
+    console.log('Trying to export workspace...')
+    var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(Blockly.Xml.domToText(xml)));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+    console.log('Exported workspace XML:' + xml);
+}
+/* I tried. This is just too over-complicated ... having to add a event listener to read a frickn plain text is just idiotic...
+function importBlocklySpace() {
+    console.log('Trying to import workspace...')
+
+    const importFiles = document.getElementById("importXMLinput")
+    const importFile = importFiles.files[0]
+    // console.log(importFile)
+
+    const reader = new FileReader()
+
+    reader.onload = function() {
+
+    }
+
+    var blarb = reader.readAsText(importFile)
+
+    console.log(blarb)
+
+
+
+    // Blockly.Xml.clearWorkspaceAndLoadFromXml(importFile.files[0], Blockly.mainWorkspace)
+    // var xml = Blockly.Xml.textToDom(importFile.files[0]);
+    // console.log('Loading workspace:' + xml);
+    // console.log(xml)
+    // Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+}
+*/
+
  (function() {
 
   let currentButton;
